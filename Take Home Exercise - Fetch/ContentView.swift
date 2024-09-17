f3912ac915cb8a8b20556dd50fee57b77e562985
holybuz <holybuz@gmail.com>
2024-09-16 23:21:58 -0700

    Initial Commit

---
+++ Take Home Exercise - Fetch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Take Home Exercise - Fetch
+//
+//  Created by Alessandro Loi on 9/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
